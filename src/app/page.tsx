3d70203095459a56ec8a2c7dcd09071bbad7e199
renanleonel <renanleonelpro@gmail.com>
2025-03-05 00:14:26 -0300

    fix: styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,19 @@ const Options = {
 
 export default function Page() {
   return (
-    <div className='flex min-h-screen items-center justify-center gap-4 bg-black'>
+    <div className='flex min-h-screen items-center justify-center gap-4 bg-[#101010]'>
       <Tabs defaultValue={Options.PURE} className='w-full max-w-md gap-10'>
         <TabsList className='w-full'>
-          <TabsTrigger value={Options.PURE} className='cursor-pointer'>
+          <TabsTrigger
+            value={Options.PURE}
+            className='cursor-pointer data-[state=active]:font-semibold'
+          >
             Pure
           </TabsTrigger>
-          <TabsTrigger value={Options.TANSTACK} className='cursor-pointer'>
+          <TabsTrigger
+            value={Options.TANSTACK}
+            className='cursor-pointer data-[state=active]:font-semibold'
+          >
             TanStack Virtual
           </TabsTrigger>
         </TabsList>
